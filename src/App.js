f7aaf8288acab2906bab8bced302f5d56cd6b602
Teodor_Valchev <teodor.valchev@fpsoftware.io>
2025-02-08 12:12:18 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name,
+    React.createElement("p", {}, props.description))
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {className: "App"},
+    [
+      React.createElement("h1", {}, "Pedro ginos"),
+      React.createElement(Pizza, {
+        name: "Pedro ginos",
+        description: "Pedro ginos",
+      }),
+      React.createElement(Pizza, {
+        name:"Mozzarella",
+        description: "my mozzarella",
+      }),
+      React.createElement(Pizza),
+      React.createElement(Pizza)
+    ]
+  )
+}
+
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App))
